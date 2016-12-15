2000924c06c759f1de96f4f1929fbdd6d730e461
Antti <antti.aromaki@gmail.com>
2016-12-15 16:39:23 +0200

    added list of tweets to keywords/show

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,4 +1,6 @@
 class Keyword < ApplicationRecord
+  has_many :tweets
+
   def grab_tweets()
     client = Twitter::REST::Client.new do |config|
       config.consumer_key        = "7bsUWtyXhKuzitwtIa5cz7GXg"
@@ -8,6 +10,16 @@ class Keyword < ApplicationRecord
     end
 
     client.search(self.word, :count => 3, :result_type => "recent").take(3).collect() do |tweet|
+      new_tweet = Tweet.new
+      new_tweet.tweet_id = tweet.id.to_s
+      new_tweet.tweet_created_at = tweet.created_at
+      new_tweet.text = tweet.text
+      new_tweet.user_uid = tweet.user.id
+      new_tweet.user_name = tweet.user.name
+      new_tweet.user_screen_name = tweet.user.screen_name
+      new_tweet.user_image_url = tweet.user.profile_image_url
+      new_tweet.keyword = self
+      new_tweet.save
       "#{tweet.user.screen_name}: #{tweet.text}"
     end
   end
